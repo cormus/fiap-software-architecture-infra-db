8fca75f9f4010cdd1685e897229fce9c97fbbd97
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-05-13 12:52:14 -0300

    Migrado main.tf do respositório fiap-software-architecture

---
+++ infra/main.tf
@@ -0,0 +1,88 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true   # Habilita DNS Resolution
+  enable_dns_hostnames = true   # Habilita DNS Hostnames
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route" "route_to_igw" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_db_subnet_group" "my_db_subnet_group" {
+  name       = "my-db-subnet-group"
+  subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
+
+resource "aws_security_group" "mysql_sg" {
+  name        = "mysql-sg"
+  description = "Allow MySQL access"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Permite acesso de qualquer IP (use com cuidado)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "mysql_db" {
+  identifier         = "my-mysql-db"
+  db_name            = var.bucket_name
+  engine             = "mysql"
+  engine_version     = "8.0"
+  instance_class     = "db.t3.micro"
+  username           = "admin"
+  password           = "582adfEt"
+  storage_type       = "gp2"
+  allocated_storage  = 20
+  skip_final_snapshot = true
+  publicly_accessible = true
+  vpc_security_group_ids = [aws_security_group.mysql_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
+}
